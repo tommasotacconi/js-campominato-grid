4f8fd16856585640074fae403b93d5018548db75
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-08-23 17:08:27 +0200

    added set up phase

--- jscript/script.js
+++ jscript/script.js
@@ -1,1 +1,12 @@
-console.log('js ok');
+// FUNZIONI
+
+// FASE DI PREPARAZIONE
+// -Prendo la griglia
+const grid = document.getElementById('grid');
+
+// -Imposto delle condizioni iniziali  per poter controllare
+// righe e colonne della griglia
+const rows = 10;
+const cols = 10;
+const totCells = rows * cols;
+
