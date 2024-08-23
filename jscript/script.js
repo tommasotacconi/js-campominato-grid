68bc95af4ffabb8d95068017c79cd0811a4295ef
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-08-23 20:18:42 +0200

    added select button and related js

--- jscript/script.js
+++ jscript/script.js
@@ -11,31 +11,107 @@ function createCell (content) {
 const grid = document.getElementById('grid');
 // -Prendo il bottone Play
 const button = document.querySelector('button');
+// -Prendo la select
+const select = document.querySelector('select');
 
 // -Imposto delle condizioni iniziali  per poter controllare
 // righe e colonne della griglia
-const rows = 10;
-const cols = 10;
-const totCells = rows * cols;
-
+const rowsBeg = 10;
+const colsBeg = 10;
+const rowsInt = 9;
+const colsInt = 9;
+const rowsExp = 7;
+const colsExp = 7;
+let totCells;
 
 // FASE DI ELABORAZIONE
 // -Attacco un event listener sul bottone play
 button.addEventListener('click', function () {
-  // -Inserisco dinamicamente le celle con un ciclo for
-  // e sfrutto la funzione create cell precedentemente creata;
-  for (let i = 0; i < totCells; i++) {
-    const cell = createCell(i + 1);
+  // -Uso uno switch per gestire i tre casi di difficoltà sulla base
+  // del valore di selectValue. Creo per ogni caso una tabella di
+  // dimensione differente
+  switch (selectValue) {
+    case 'lvl-1':
+      // Calcolo la variabile totCells
+      totCells = rowsBeg * colsBeg;
 
-    // -Attacco un event listener alla cella che faccia cambiare
-    // il suo colore quando cliccata e che stampi in console il suo
-    // numero
-    cell.addEventListener('click', function () {
-      cell.classList.toggle('clicked');
-      console.log(cell.innerText);
-    })
-  
-    // -Inserisco la cella in pagina
-    grid.appendChild(cell);
+      // -Inserisco dinamicamente le celle con un ciclo for
+      // e sfrutto la funzione createCell() precedentemente creata;
+      for (let i = 0; i < totCells; i++) {
+        const cell = createCell(i + 1);
+
+        // -Inserisco la classe che dimensiona la cella
+        cell.classList.add('beginner');
+        
+        // -Attacco un event listener alla cella che faccia cambiare
+        // il suo colore quando cliccata e che stampi in console il suo
+        // numero
+        cell.addEventListener('click', function () {
+          cell.classList.toggle('clicked');
+          console.log(cell.innerText);
+        })
+        
+        // -Inserisco la cella in pagina
+        grid.appendChild(cell);
+      }
+      break;
+      case 'lvl-2':
+        // Calcolo la variabile totCells
+        totCells = rowsInt * colsInt;
+        
+        // -Inserisco dinamicamente le celle con un ciclo for
+        // e sfrutto la funzione createCell() precedentemente creata;
+        for (let i = 0; i < totCells; i++) {
+          const cell = createCell(i + 1);
+          
+          // -Inserisco la classe che dimensiona la cella
+          cell.classList.add('intermediate');
+          
+          // -Attacco un event listener alla cella che faccia cambiare
+          // il suo colore quando cliccata e che stampi in console il suo
+          // numero
+          cell.addEventListener('click', function () {
+            cell.classList.toggle('clicked');
+            console.log(cell.innerText);
+          })
+          
+          // -Inserisco la cella in pagina
+        grid.appendChild(cell);
+      }
+      break;
+      case 'lvl-3':
+        // Calcolo la variabile totCells
+        totCells = rowsExp * colsExp;
+        
+        // -Inserisco dinamicamente le celle con un ciclo for
+        // e sfrutto la funzione createCell() precedentemente creata;
+        for (let i = 0; i < totCells; i++) {
+          const cell = createCell(i + 1);
+
+          // -Inserisco la classe che dimensiona la cella
+          cell.classList.add('expert');
+          
+          // -Attacco un event listener alla cella che faccia cambiare
+          // il suo colore quando cliccata e che stampi in console il suo
+          // numero
+          cell.addEventListener('click', function () {
+            cell.classList.toggle('clicked');
+            console.log(cell.innerText);
+        })
+      
+        // -Inserisco la cella in pagina
+        grid.appendChild(cell);
+      }
+      break;
+      default:
+        alert('Seleziona un livello');
   }
+});
+
+// -Attacco un event listener alla select per modificare il valore
+// di una variabile che tenga conto del valore della select preso
+let selectValue;
+select.addEventListener('change', function () {
+  selectValue = select.value;
+  console.log(selectValue);
 });
